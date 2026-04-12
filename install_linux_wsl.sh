9183f769c16c79fe73794395b4f8f9fb9ce9f30c
simaxme <contact@simax.me>
2026-04-12 19:50:52 +0200

    Remove catppuccin-kitty submodule
    This commit removes the catppuccin kitty theme repository
    as a submodule and manually adds the corresponding theme
    files and LICENSE notice to `kitty/catppuccin`.

--- install_linux_wsl.sh
+++ install_linux_wsl.sh
@@ -1,6 +1,3 @@
-# Initialize git
-bash scripts/init_git_submodules.sh
-
 # Simple script that will install all the dotfiles in the appropriate directory
 cp ./.tmux.conf ~/
 cp ./.zshrc ~/
--- install_macos.sh
+++ install_macos.sh
@@ -1,6 +1,3 @@
-# Initialize git
-bash scripts/init_git_submodules.sh
-
 # Simple script that will install all the dotfiles in the appropriate directory
 cp ./.ideavimrc ~/
 cp ./.tmux.conf ~/
@@ -9,5 +6,3 @@ cp ./global.gitignore ~/.gitignore
 
 # Kitty
 cp -r ./kitty ~/.config/
-cp ./external_repos/catppuccin-kitty/themes/diff-mocha.conf ~/.config/kitty/theme.conf
-cat ./external_repos/catppuccin-kitty/themes/mocha.conf >> ~/.config/kitty/theme.conf
