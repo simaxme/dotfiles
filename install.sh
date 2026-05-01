4c89d41a69f6f279424e36f1cc739ba107c6b693
simaxme <contact@simax.me>
2026-05-01 14:53:52 +0200

    Use display name instead of id in sway output config

--- install.sh
+++ install.sh
@@ -24,7 +24,8 @@ else
   else
     # Linux (Not WSL)
     cp -r ./pipewire ~/.config
-    cp -r ./sway ~/.config
+    mkdir -p ~/.config/sway
+    cat ./sway/config | sed "s/%DISPLAY_NAME%/'$(cat display.txt || echo DP-7)'/g" > ~/.config/sway/config
     cp -r ./swaylock ~/.config
     cp -r ./swayidle ~/.config
     cp -r ./waybar ~/.config
