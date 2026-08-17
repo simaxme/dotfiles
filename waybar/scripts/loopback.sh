adec809b5471c3882172895e3b93a49e3063f05b
simaxme <contact@simax.me>
2026-08-17 12:32:28 +0200

    Revert "Add loopback button to waybar"
    This reverts commit 127ca8b3981592c7b699297c4e3ed8807c6c74ec.

--- waybar/scripts/loopback.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PGREP_RESPONSE=$(pgrep -x pw-loopback)
-
-if [ -n "$PGREP_RESPONSE" ]; then
-  echo '{"text": "󱡬 '"$(echo "$PGREP_RESPONSE" | wc -l)"'", "class": "loopback-active"}'
-else
-  echo '{"text": "󱡬", "class": "loopback-inactive"}'
-fi
--- waybar/scripts/start-loopback.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if pgrep -x --quiet pw-loopback; then
-  killall pw-loopback
-else
-  setsid pw-loopback &> /dev/null < /dev/null &
-fi
